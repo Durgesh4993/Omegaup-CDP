085254366e8430a5c3330a31bc69bae3228bbcb8
Durgesh kumar prajapati <98798977+Durgesh4993@users.noreply.github.com>
2024-03-03 05:42:19 +0530

    Update useInputPage.ts

--- src/hooks/useInputPage.ts
+++ src/hooks/useInputPage.ts
@@ -1,51 +1,49 @@
-import { useEffect, useState } from "react";
-import {   ILine } from "../redux/models/input/inputInterfaces";
+import { useEffect, useState, useMemo } from "react";
+import { ILine } from "../redux/models/input/inputInterfaces";
 import { useStoreActions, useStoreState } from "../redux/store";
 import { ICase } from "../redux/models/cases/casesInterfaces";
 import _ from "lodash";
 import { uuid } from "uuidv4";
 
-// Loads/Creates an Input Page base on the selected case
 export const useInputPage = (caseData: ICase) => {
   const [pageData, setPageData] = useState<ILine[]>([]);
 
+  // Select relevant data from Redux store
   const inputData = useStoreState((state) => state.input.data);
   const layout = useStoreState((state) => state.input.layout);
   const selected = useStoreState((state) => state.cases.selected);
 
+  // Define action to add input page to store
   const addInputPage = useStoreActions((actions) => actions.input.addData);
 
-  const caseIdentifier = {
-    groupId: caseData.groupId,
-    caseId: caseData.caseId,
-  };
+  // Memoize the caseIdentifier object to avoid recreating it on every render
+  const caseIdentifier = useMemo(
+    () => ({
+      groupId: caseData.groupId,
+      caseId: caseData.caseId,
+    }),
+    [caseData]
+  );
 
   useEffect(() => {
+    // Check if a case is selected
     if (selected.caseId === "None" || selected.groupId === "None") return;
 
+    // Find input page corresponding to the current case
     const inputPage = inputData.find((inputElement) => {
       return _.isEqual(inputElement.id, caseIdentifier);
     });
 
-    // Page doesn't exit, create one
-    if (inputPage === undefined) {
-      // Layout is not defined
-      if (layout !== undefined) {
-        // Create new random id's so they don't overlap
-        const layoutNewIds = layout.map((layoutElement) => {
-          layoutElement.lineId = uuid();
-          return layoutElement;
-        });
-        addInputPage({ id: caseIdentifier, lines: layoutNewIds, outData: "" });
-        setPageData(layoutNewIds);
-      } else {
-        addInputPage({ id: caseIdentifier, lines: [], outData: "" });
-        setPageData([]);
-      }
+    if (!inputPage) {
+      // If input page doesn't exist, create one
+      const newLines = layout ? layout.map((element) => ({ ...element, lineId: uuid() })) : [];
+      addInputPage({ id: caseIdentifier, lines: newLines, outData: "" });
+      setPageData(newLines);
     } else {
+      // If input page exists, set page data from store
       setPageData(inputPage.lines);
     }
-  }, [caseData, inputData]);
+  }, [caseIdentifier, inputData, layout, selected]);
 
   return { pageData, setPageData };
 };
